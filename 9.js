8795e1ab1f54abb2bd382abff8c87d1f8c8c79a7
JJIMINSHIN <scalla1031@gmail.com>
2022-06-02 21:46:20 +0900

    concat을 활용한 출력 방법

---
+++ 9.js
@@ -0,0 +1,13 @@
+let year = '2019';
+let month = '04';
+let day = '26';
+let hour = '11';
+let minute = '34';
+let second = '27';
+
+let result = year.concat("/", month, "/", day, " ", hour, ":", minute, ":", second);
+
+console.log(result);
+
+
+//concat은 사슬같이 변수를 하나씩 이어서 출력할 수 있게 해주는 메서드
