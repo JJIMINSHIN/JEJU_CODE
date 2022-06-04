4d9e81becd5243122ce0353e37e01c3e2609fc28
JJIMINSHIN <scalla1031@gmail.com>
2022-06-04 19:06:37 +0900

    놀이기구 키 제한

--- 16.js
+++ 16.js
@@ -2,5 +2,3 @@
 let a = prompt("문장을 입력해 주세요");
 a.split('').reverse().join('')
 
-
-//n.reverse is not a function
---
+++ 17.js
@@ -0,0 +1,7 @@
+const n = prompt("키를 입력해 주세요");
+
+if(n>=150){
+    console.log('YES');
+}else{
+    console.log('NO');   
+}
