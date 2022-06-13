2e5dda66376e0b11b4eb10861586f78ccbeb1868
JJIMINSHIN <scalla1031@gmail.com>
2022-06-13 21:39:09 +0900

    문제26 : 행성 문제2

--- 26.js
+++ 26.js
@@ -1,12 +1,7 @@
 function plants(plant){
-    let plant1 = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+    let plant1 = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
     let plant2 = ['수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성'];
 
-    // for(let i =0; i<plant1.length; i++){
-    //     if(plant2[i] == plant){
-    //         console.log(plant1);
-    //     }
-    // }
     plant1.forEach(function(item){
         if(plant2[item] == plant){
             console.log(plant1);
@@ -14,6 +9,5 @@ function plants(plant){
     })
 
 }
-
 
 plants('지구')
