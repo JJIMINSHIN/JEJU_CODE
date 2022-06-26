4003ec48fa465990d43aaad6acc453eb1d92e113
JJIMINSHIN <scalla1031@gmail.com>
2022-06-26 15:57:27 +0900

    문제 18 : 평균 점수

--- 18.js
+++ 18.js
@@ -1,7 +1,7 @@
-const input = prompt('과목별 점수를 공수를 입력해주세요 20 30 40 ').split('');
+const input = prompt('과목별 점수를 공수를 입력해주세요 20 30 40 ').split(' ');
 let sum =0;
 
 for(let i=0; i<3; i++){
-    sum += parseInt(input, 10);
+    sum += parseInt(input[i], 10);
 }  
-console.log(math.floor(sum/3));
+console.log(Math.floor(sum/3));
