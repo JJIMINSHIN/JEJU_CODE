0e618feab194169b3197395720988f26e846f892
JJIMINSHIN <scalla1031@gmail.com>
2022-06-26 16:09:49 +0900

    문제 19 : 제곱을 구하자

--- 19.js
+++ 19.js
@@ -1,4 +1,9 @@
-const n =prompt("두 수를 입력해주세요.").split(" ");
-let a = parseInt(n[0], 10);
-let b = parseInt(n[1], 10);
-console.log(Math.pow(a,b));
+function pow(num, a, b){
+    num = prompt("두 수를 입력해주세요.").split(" ");
+    a = Number(num[0]);
+    b = Number(num[1]);
+    
+    console.log(Math.pow(a,b))
+}
+
+pow()
