522cbb72985200049b5850ccced7cb5643e9232f
JJIMINSHIN <scalla1031@gmail.com>
2022-06-26 16:56:46 +0900

    문제27 : 객체 만들기

--- 27.js
+++ 27.js
@@ -1,11 +1,10 @@
-let name = prompt('이름을 입력해 주세요(영어)').split(' ');
+let names = prompt('이름을 입력해 주세요(영어)').split(' ');
 let score = prompt('점수를 입력해주세요').split(' ');
 
 const obj = {};
 
-for(let i =0; i<name.length; i++){
-    obj[name[i]] = parseInt(score[i], 10);
+for(let i =0; i<names.length; i++){
+    obj[names[i]] = Number(score[i]);
 }
 
-console.log(obj)
-
+console.log(obj);
