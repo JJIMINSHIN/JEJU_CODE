f02fb6283d061b5684deea6fef8bb1d6b0732933
JJIMINSHIN <scalla1031@gmail.com>
2022-06-26 16:20:16 +0900

    문제 20 : 몫과 나머지

--- 20.js
+++ 20.js
@@ -1,7 +1,13 @@
-const n =prompt(" 두 수를 입력해 주세요.").split(" ");
-let a = parseInt(n[0], 10);
-let b = parseInt(n[1], 10);
-let mok = math.floor(a/b);
-let left = a%b;
+function cal(num, a, b){
+    num = prompt('').split(' ');
+    a = Number(num[0]);
+    b = Number(num[1]);
 
-console.log(mok+" "+ left)
+    let mok = Math.floor(a/b);
+    let left = a%b;
+
+    console.log(`목은 ${mok}이고 나머지는 ${left}이다.`);
+
+}
+
+cal()
