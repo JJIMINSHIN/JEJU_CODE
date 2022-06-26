dccc3ee4199f915b5bc5782152aad1438e3343e2
JJIMINSHIN <scalla1031@gmail.com>
2022-06-26 16:40:09 +0900

    문제 25 : 원의 넓이를 구하세요

--- 25.js
+++ 25.js
@@ -1,7 +1,7 @@
 function circle(r){
-    let r = r * r * 3.14;
+    r = r * r * 3.14;
     return r;
 }
 
-const input =prompt('반지름의 길이를 입력해주세요.');
-console.log(circle(input))
+const input =Number(prompt('반지름의 길이를 입력해주세요.'));
+console.log(circle(input));
